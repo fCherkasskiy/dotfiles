7d945fccf9e7b1a624dadc919e1677d78c29895e
Felix Cherkasskiy <= fcherkasskiy@gmail.com>
2026-07-19 21:21:40 -0400

    Removed XDG_DATA_DIRS, XDG_CONFIG_DIRS, and XDG_RUNTIME_DIR from xdg-vars.sh, encountering issues with flatpaks not being automatically added to the path, simplified alacritty config

--- config/shellenv/xdg-vars.sh
+++ config/shellenv/xdg-vars.sh
@@ -3,14 +3,15 @@
 if [[ "$OSTYPE" == darwin* ]]; then
   export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
   #export HOME=/Users/fcher
-  export XDG_DATA_DIRS=~/.local/share
-  export XDG_CONFIG_DIRS=~/.config
-  export XDG_RUNTIME_DIR=~/Library/Application\ Support
+  # export XDG_DATA_DIRS=~/.local/share
+  # export XDG_CONFIG_DIRS=~/.config
+  # export XDG_RUNTIME_DIR=~/Library/Application\ Support
 else
   #export HOME=/home/fcher
-  export XDG_DATA_DIRS=/usr/local/share:/usr/share
-  export XDG_CONFIG_DIRS=/etc/xdg
-  export XDG_RUNTIME_DIR=/run/user/$UID
+  # export XDG_DATA_DIRS=/usr/local/share:/usr/share
+  # export XDG_CONFIG_DIRS=/etc/xdg
+  # export XDG_RUNTIME_DIR=/run/user/$UID
+  true
 fi
 
 export XDG_CONFIG_HOME=~/.config
